4ecf4008401267fa52b3486d3ccf0b884012146a
Adam Riddell <adamr@catalyst-au.net>
2015-04-01 10:56:37 +1100

    Update save script to work with new format
            save.sh

--- save.sh
+++ save.sh
@@ -1,21 +1,24 @@
 #! /bin/bash
 ## Check that the containers are running
 running=1
-for CONTAINER in "$webcont" "$dbcont"
+for IMAGE in "${!contnames[@]}"
     do
+        CONTAINER=${contnames[$IMAGE]}
         if ! `sudo docker ps | grep --quiet $CONTAINER`
             then
                 echo "$CONTAINER is not running!"
                 running=0
         fi
     done
-## If both containers are running, commit them
+## If all containers are running, commit them
 if [ $running = 1 ]
     then
-       echo "Committing web image"
-       sudo docker commit $webcont $dockreg/$webimage
-       echo "Committing db image"
-       sudo docker commit $dbcont $dockreg/$dbimage
+        for IMAGE in "${!contnames[@]}"
+            do
+                CONTAINER=${contnames[$IMAGE]}
+                echo "Committing $IMAGE"
+                sudo docker commit $CONTAINER $dockreg/$IMAGE
+            done
 else
        echo "Aborting save: one or more containers are not running."
 fi
