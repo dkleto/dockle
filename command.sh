2cd438a922aef5f4c1d48e9856515b47126c66b7
Adam Riddell <riddelladam42@gmail.com>
2015-06-26 09:02:20 +1000

    Base command on first argument
        command.sh:
            - Previously, the command.sh would iterate through all
              args, looking for a valid command. Now it will assume
              that the first argument is the intended command.

--- command.sh
+++ command.sh
@@ -5,38 +5,39 @@
 
 ## Run the approprate script based on arguments.
 ## This script is intended to be run from the parent dir.
-for ARG in $*
-    do
-        case $ARG in
-            start)
-            . ./scripts/start.sh
-            exit
-            ;;
-            stop)
-            . ./scripts/stop.sh
-            exit
-            ;;
-            restart)
-            . ./scripts/stop.sh
-            . ./scripts/start.sh
-            exit
-            ;;
-            psql)
-            . ./scripts/psql.sh
-            exit
-            ;;
-            pg_dump)
-            . ./scripts/psql.sh -d
-            exit
-            ;;
-            save)
-            . ./scripts/save.sh
-            exit
-            ;;
-            *)
-            continue
-        esac
-    done
+case $1 in
+    start)
+    . ./scripts/start.sh
+    exit
+    ;;
+    stop)
+    . ./scripts/stop.sh
+    exit
+    ;;
+    restart)
+    . ./scripts/stop.sh
+    . ./scripts/start.sh
+    exit
+    ;;
+    psql)
+    . ./scripts/psql.sh
+    exit
+    ;;
+    pg_dump)
+    . ./scripts/psql.sh -d
+    exit
+    ;;
+    save)
+    . ./scripts/save.sh
+    exit
+    ;;
+    build)
+    exit
+    . ./scripts/builddb.sh
+    ;;
+    *)
+    continue
+esac
 
 ## Start environment if no arguments passed.
 . ./scripts/start.sh
